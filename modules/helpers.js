3b833c757a2c185b719008204736a364ef7cdd10
thecoder219 <gamingbro.881819@gmail.com>
2025-11-13 16:01:29 +0530

    Delete modules directory

--- modules/helpers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { SANITIZE_TOKEN } from './constants.js';
-
-const pickVariant = list => list[Math.floor(Math.random() * list.length)];
-const sanitizeStreamText = text => (text || '').replace(SANITIZE_TOKEN, '');
-
-export { pickVariant, sanitizeStreamText };
